9d56509d77d8f5b00148bd68e7c1befd8392f6df
Aditi Saxena <126320375+Aditi170104@users.noreply.github.com>
2025-12-31 01:00:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import store from './store'
 import { Provider } from 'react-redux'
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-paste-spa">
-<<<<<<< HEAD
+      {/* <BrowserRouter basename="/PasteNest"> */}
         <App />
         <Toaster />
-=======
-      <Toaster/>
->>>>>>> c74cf64 (Made UI responsive and updated layout styles)
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
