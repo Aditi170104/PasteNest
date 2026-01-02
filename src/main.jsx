13672bb4263a2135bfa6efdbf4422af685e9f14d
Aditi Saxena <126320375+Aditi170104@users.noreply.github.com>
2026-01-02 23:03:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import store from './store'
 import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
-
+const basename = import.meta.env.BASE_URL;
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter basename="/PasteNest"> */}
         <App />
         <Toaster />
-      {/* </BrowserRouter> */}
     </Provider>
   </StrictMode>
 )
